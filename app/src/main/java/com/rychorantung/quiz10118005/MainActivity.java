efa3797841c156971489be8b8fe123aa0e0fc4b8
RychoRantung <rantungrico@gmail.com>
2021-05-29 13:55:40 +0800

    Initial Commit

---
+++ app/src/main/java/com/rychorantung/quiz10118005/MainActivity.java
@@ -0,0 +1,21 @@
+package com.rychorantung.quiz10118005;
+/*
+ * Nama: Rycho Rantung
+ * NIM: 10118005
+ * Kelas: IF-1
+ * Tanggal Pengerjaan: 29 Mei 2021
+
+ */
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
